694e6bc82363d7e3dcc3653f69f6fc2ed37c5d2f
DSTechLabs <bill@dstechlabs.com>
2024-06-07 15:15:16 -0400

    Initial Commit

---
+++ Scripts/appScript.js
@@ -0,0 +1,57 @@
+//=========================================================
+//
+//     FILE : appScript.js
+//
+//  PROJECT : Responsive Mobile Web App Template
+//
+//   AUTHOR : Bill Daniels
+//            Copyright 2014-2024, D+S Tech Labs, Inc.
+//            All Rights Reserved
+//
+//=========================================================
+
+//--- Globals ---------------------------------------------
+
+
+
+
+
+//--- Initial JS ------------------------------------------
+
+try
+{
+  // Browser stuff
+  window.devicePixelRatio = 1.0;  // set pixel density to 1
+
+  // Splash is showing, so hide appBar and navBar
+  $("#appBar").hide ();
+  $("#navBar").hide ();
+
+  // Load all pages
+  LoadContentPages ();
+}
+catch (ex)
+{
+  ShowException (ex);
+}
+
+//--- Startup ---------------------------------------------
+
+function Startup ()
+{
+  try
+  {
+    // Show appBar and navBar
+    $("#appBar").show ();
+    $("#navBar").show ();
+
+    // Click on first navButton
+    // $(".navButton").first().mousedown();  // Not working for pointerdown; no pointerdown() method in jQuery ???
+    firstNavButton = document.getElementsByClassName ('navButton')[0];
+    ShowPage ('home', firstNavButton);
+  }
+  catch (ex)
+  {
+    ShowException (ex);
+  }
+}
